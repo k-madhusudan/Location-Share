66fb1ff3edb8680606b37273faa9d4f3c2d83d1d
Madhusudan K <k-madhusudan@users.noreply.github.com>
2015-10-03 20:38:00 +0530

    Read locations from Android Location API

--- app/src/main/java/com/example/madhusudan/locationshare/HomeScreen.java
+++ app/src/main/java/com/example/madhusudan/locationshare/HomeScreen.java
@@ -1,17 +1,51 @@
 package com.example.madhusudan.locationshare;
 
+import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 
-public class HomeScreen extends ActionBarActivity {
+public class HomeScreen extends Activity implements LocationListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+
+        ConnectivityManager cm= (ConnectivityManager)getSystemService(getApplicationContext().CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo=cm.getActiveNetworkInfo();
+
+        if(cm.getActiveNetworkInfo()==null || !cm.getActiveNetworkInfo().isConnected()){
+            Toast.makeText(getApplicationContext(),"Connect to internet please",Toast.LENGTH_LONG).show();
+        }
+        else{
+           // Toast.makeText(getApplicationContext(),"Hello World!",Toast.LENGTH_LONG).show();
+       }
+
+        LocationManager locationManager= (LocationManager) getSystemService(getApplicationContext().LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(locationManager.NETWORK_PROVIDER,60000,1,this);
+        Location loc= locationManager.getLastKnownLocation(locationManager.NETWORK_PROVIDER);
+        TextView tv= (TextView) findViewById(R.id.textView);
+        TextView tv2= (TextView) findViewById(R.id.textView2);
+        tv.setText(Double.toString(loc.getLatitude()));
+        tv2.setText(Double.toString(loc.getLongitude()));
+
+        onLocationChanged(loc);
+
+
     }
 
     @Override
@@ -34,5 +68,25 @@ public class HomeScreen extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Toast.makeText(getApplicationContext(),"Loc:"+location.getLatitude(),Toast.LENGTH_LONG);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(getApplicationContext(),"Enabled",Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
